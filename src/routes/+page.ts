d00d0f3de84e392cf430c38b3f84b804fb665d42
hansfigo <hambasahaya0303@gmail.com>
2023-06-17 21:08:21 +0700

    API fix

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -2,7 +2,7 @@ import { error } from '@sveltejs/kit';
 
 /** @type {import('./$types').PageLoad} */
 export async function load({ params }) {
-    const data = await fetch("https://api.waifu.im/search/?included_tags=oppai&many=true");
+    const data = await fetch("https://api.waifu.im/search/?many=true");
     
     return await data.json()
 
